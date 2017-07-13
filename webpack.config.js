12b52a55690e72acd156f7684eaf86ff0fc8f751
ranjanvivek1994@gmail.com <vivek.ranjan@traveltriangle.com>
2017-07-13 22:54:01 +0530

    config changes for sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ var config = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
   plugins: [new HtmlWebpackPlugin({
