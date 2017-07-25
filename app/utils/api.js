ab9dac7726b760f0be05550785d3605092b9ff8e
ranjanvivek1994@gmail.com <vivek.ranjan@traveltriangle.com>
2017-07-25 14:19:00 +0530

    endpoint changed

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
+import config from './api_config';
 
 module.exports = {
 	fetchAllPosts: function(){
-		var encodedURI = window.encodeURI('http://localhost:3038/api/posts');
+		var encodedURI = window.encodeURI(`http://${config.api_host}:3038/api/posts`);
 		return axios.get(encodedURI)
 			.then(function(response){
 				return response.data;
 			});
 	},
 	fetchPostById: function(postId){
-		var encodedURI = window.encodeURI(`http://localhost:3038/api/posts/${postId}`);
+		var encodedURI = window.encodeURI(`http://${config.api_host}:3038/api/posts/${postId}`);
 		return axios.get(encodedURI)
 			.then(function(response){
 				return response.data;
 			});	
 	},
 	createNewPost: function(postData){
-		var encodedURI = window.encodeURI(`http://localhost:3038/api/posts`);
+		var encodedURI = window.encodeURI(`http://${config.api_host}:3038/api/posts`);
 		return axios.post(encodedURI, postData)
 			.then(function(response){
 				return response.data;
 			});		
 	},
 	updateCurrentPost: function(id, postData){
-		var encodedURI = window.encodeURI(`http://localhost:3038/api/posts/${id}`);
+		var encodedURI = window.encodeURI(`http://${config.api_host}:3038/api/posts/${id}`);
 		return axios.put(encodedURI, postData)
 			.then(function(response){
 				return response.data;
---
+++ app/utils/api_config.js
@@ -0,0 +1,6 @@
+var config = {
+	api_host: "vivekranjan.info",
+	api_port: "3038"
+};
+
+module.exports = config;
